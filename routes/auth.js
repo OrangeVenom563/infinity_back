cb6660a9207b753cb1ca5b91d985203ffc511b42
Lawrence J <lawrenceg563@gmail.com>
2021-09-10 13:12:44 +0530

    created requireLogin controller to verify JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
+const requireLogin = require('../controllers/requireLogin')
 
 router.get("/", (req, res) => {
   res.send("hello");
@@ -62,5 +63,9 @@ router.post('/signin',(req,res) => {
         })
     })
 })
+
+router.get('/protected',requireLogin,(req,res)=>{
+    res.send("You have succesfully implemented JWT authentication");
+});
 
 module.exports = router;

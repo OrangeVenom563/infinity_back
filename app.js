18104b4e1cfe7eb6a67722f45be02984ef933763
Lawrence J <lawrenceg563@gmail.com>
2021-09-15 19:47:35 +0530

    configured for heroku deployment

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = 5000;
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI);
 
